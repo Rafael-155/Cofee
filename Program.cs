74e8e780696bde8591ff6cbec187303418291dad
Rafael-155 <ernesto_vera@usmp.pe>
2024-06-14 23:20:16 -0500

    Roles

--- Program.cs
+++ Program.cs
@@ -14,6 +14,11 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+
+    //AGREGANDO ROLES
+    .AddRoles<IdentityRole>()
+
+
     .AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddControllersWithViews();
 
